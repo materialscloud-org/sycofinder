2569b08efb4a0275de16439fc93224440b76b6bd
Leopold Talirz <leopold.talirz@gmail.com>
2018-07-04 21:57:22 +0200

    add field for number of samples

--- app.py
+++ app.py
@@ -34,20 +34,6 @@ weight_range = [-1, 1]
 grid_points = 5
 
 
-def generate_table(dataframe, max_rows=10):
-    return html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])] +
-
-        # Body
-        [
-            html.Tr([
-                html.Td('{:.2f}'.format(dataframe.iloc[i][col]))
-                for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-
-
 # pylint: disable=redefined-builtin
 def get_controls(id, desc, range, default_weight=0.0):
     """Get controls for one variable.
@@ -103,7 +89,14 @@ weight_states = [
     dash.dependencies.State(k + "_weight", 'value') for k in labels
 ]
 
-ninps = len(low_states + high_states + weight_states)
+inp_nsamples = html.Div([
+    html.Span('Number of samples: '),
+    dcc.Input(
+        id='nsamples', type='number', value=20, className="nsamples range")
+])
+nsamples_state = dash.dependencies.State('nsamples', 'value')
+
+ninps = len(low_states + high_states + weight_states) + 1
 
 btn_compute = html.Div([
     html.Button('compute', id='btn_compute'),
@@ -119,7 +112,7 @@ app.css.config.serve_locally = True
 app.layout = html.Div([
     css,
     controls_html,
-    #inputs_html,
+    inp_nsamples,
     btn_compute,
     #graph, hover_info,
     #click_info
@@ -141,7 +134,7 @@ for k, v in controls_dict.iteritems():
 @app.callback(
     dash.dependencies.Output('compute_info', 'children'),
     [dash.dependencies.Input('btn_compute', 'n_clicks')],
-    low_states + high_states + weight_states)
+    low_states + high_states + weight_states + [nsamples_state])
 # pylint: disable=unused-argument
 def on_compute(out, *args):
     """Callback for clicking compute button"""
@@ -152,14 +145,15 @@ def on_compute(out, *args):
     low_vals = [args[i] for i in range(nq)]
     high_vals = [args[i + nq] for i in range(nq)]
     weight_vals = [args[i + 2 * nq] for i in range(nq)]
+    nsamples = args[-1]
 
     samples = uniform.compute(
         var_importance=weight_vals,
         var_LB=low_vals,
         var_UB=high_vals,
+        num_samples=nsamples,
     )
     df = pd.DataFrame(data=samples, columns=labels)
-    pd.options.display.float_format = '{:.2f}'.format
 
     return generate_table(df)
 
@@ -171,6 +165,20 @@ def on_compute(out, *args):
     #        var_LB=low_vals.values()[:nvars],
     #        var_UB=high_vals.values()[:nvars],
     #        )
+
+
+def generate_table(dataframe, max_rows=100):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [
+            html.Tr([
+                html.Td('{:.2f}'.format(dataframe.iloc[i][col]))
+                for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
 
 
 if __name__ == '__main__':
