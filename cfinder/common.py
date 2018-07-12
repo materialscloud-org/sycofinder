230dbdb8134977fc38253bdddd14c18a05670b11
Leopold Talirz <leopold.talirz@gmail.com>
2018-07-12 20:07:15 +0200

    add compute button to ga/ml
     * ga & ml now have a compute button
     * fixed title of app
     * hide graph in ml

--- cfinder/common.py
+++ cfinder/common.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=wrong-import-position
+# pylint: disable=wrong-import-position,import-error,multiple-imports
 from __future__ import print_function
 from builtins import range
 from future import standard_library
 standard_library.install_aliases()
 import urllib.request, urllib.parse, urllib.error
 import dash_html_components as html
+import dash_table_experiments as dt
 import base64
 import io
 import pandas as pd
 import numpy as np
 
 
+def render_df(df):
+    return html.Div([
+        # Use the DataTable prototype component:
+        # github.com/plotly/dash-table-experiments
+        dt.DataTable(rows=df.to_dict('records')),
+    ])
+
+
 def generate_table(dataframe, max_rows=100, download_link=False):
-    table = html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])] +
 
-        # Body
-        [
-            html.Tr([
-                html.Td(cell_format(dataframe.iloc[i][col]))
-                for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-
+    components = []
     if download_link:
         csv_string = dataframe.to_csv(
             index=False, encoding='utf-8', float_format='%.2f')
@@ -33,10 +32,24 @@ def generate_table(dataframe, max_rows=100, download_link=False):
             download="synthesis_conditions.csv",
             href="data:text/csv;charset=utf-8," +
             urllib.parse.quote(csv_string),
-            target="_blank")
-        table = [table, link]
+            target="_blank",
+            className='button')
+        components.append(link)
 
-    return table
+    components.append(
+        html.Table(
+            # Header
+            [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+            # Body
+            [
+                html.Tr([
+                    html.Td(cell_format(dataframe.iloc[i][col]))
+                    for col in dataframe.columns
+                ]) for i in range(min(len(dataframe), max_rows))
+            ]))
+
+    return components
 
 
 def cell_format(value):
@@ -70,10 +83,15 @@ def validate_df(df):
         raise ValueError("Last column needs to be 'fitness', got {}".format(
             row_titles[-1]))
 
-    if set(df.dtypes.values) != set([np.dtype('float64'),np.dtype('int64')]):
+    if not set(df.dtypes.values) < set(
+        [np.dtype('float64'), np.dtype('int64')]):
         raise ValueError(
             "All values must be floats, got data tpes\n {}".format(df.dtypes))
 
+
+# styles
+HIDE = {'display': 'none'}
+SHOW = {}
 
 #app.css.append_css({
 #    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
