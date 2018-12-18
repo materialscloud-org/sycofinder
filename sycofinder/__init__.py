452bcd07136d91a6ec9db41807e04b455811f0de
Leopold Talirz <leopold.talirz@gmail.com>
2018-12-18 23:33:14 +0100

    try fixing dash server cfg

--- sycofinder/__init__.py
+++ sycofinder/__init__.py
@@ -1,6 +1,7 @@
 import dash
 
-app = dash.Dash(url_base_pathname='/sycofinder/')
+app = dash.Dash()
+#app = dash.Dash(url_base_pathname='/sycofinder/')
 #app = dash.Dash(__name__, static_folder='static')
 server = app.server
 app.config.suppress_callback_exceptions = True
