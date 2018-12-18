46ba7a78b23d7c98c4d7b322b048496e70084279
Leopold Talirz <leopold.talirz@gmail.com>
2018-12-19 00:16:55 +0100

    fix dash name

--- sycofinder/__init__.py
+++ sycofinder/__init__.py
@@ -1,8 +1,8 @@
 import dash
 
-app = dash.Dash()
+app = dash.Dash(__name__)
+#app = dash.Dash()
 #app = dash.Dash(url_base_pathname='/sycofinder/')
-#app = dash.Dash(__name__, static_folder='static')
 server = app.server
 app.config.suppress_callback_exceptions = True
 app.scripts.config.serve_locally = True
