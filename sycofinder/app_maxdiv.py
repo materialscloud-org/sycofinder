0e1732bf1e4f4dcd5569b464ff048d60318084d7
Leopold Talirz <leopold.talirz@gmail.com>
2019-01-12 12:28:45 +0100

    reduce nvars_max for minmax to 6

--- sycofinder/app_maxdiv.py
+++ sycofinder/app_maxdiv.py
@@ -33,7 +33,8 @@ variables = collections.OrderedDict([
 NVARS_DEFAULT = len(variables)
 
 # Fill up to NVARS_MAX (needed to define callbacks)
-NVARS_MAX = 20
+# Note: In the current implementation, anythin beyond 6 takes too much time
+NVARS_MAX = 6
 for i in range(len(variables), NVARS_MAX):
     k = 'variable_{}'.format(i + 1)
     variables[k] = dict(label=k, range=[0, 1], weight=1)
